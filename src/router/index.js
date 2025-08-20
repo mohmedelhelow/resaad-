e5e9b6de7f1f51e9a02b5634b7e188df4a5e5f0f
mohamedelhelow <mohammed.elhelow3@gmail.com>
2025-08-20 04:10:14 +0300

    hashsa

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import AboutUs from '../views/AboutUs.vue'
 import Services from '../views/OurServices.vue'
 import ContactUs from '../views/ContactUs.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/',        name: 'home',     component: Home,      meta: { titleKey: 'title.home' } },
     { path: '/about',   name: 'about',    component: AboutUs,   meta: { title: 'About Us' } },
