999eea590bed97a4d1c8bb2f0d8d249ff56fb08a
mohamedelhelow <mohammed.elhelow3@gmail.com>
2025-08-26 11:23:10 +0300

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap' // 👈 default export
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
     vue(),
     vueDevTools(),
+    Sitemap({
+      hostname: 'https://www.resaad.com',
+      // لو لسه ما عندكش توليد تلقائي للروتس، اكتب الأساسيين مؤقتًا:
+     routes: ['/', '/about', '/services', '/contact'],
+
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
